a0492d9720df175e4f88dbcf8e064167de7b9765
KALANA <kalanabimsaradj5@gmail.com>
2023-06-24 18:45:03 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
